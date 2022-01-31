d7dd2a67544c04fba25d4bafba76cd62fade87bc
unknown <manan@protonmail.com>
2022-01-31 09:13:16 +0530

    '-done'

--- backend/dashboardBackend.js
+++ backend/dashboardBackend.js
@@ -3,23 +3,21 @@ import Constants from "../config";
 import { AsyncStorage } from "react-native";
 
 class DashboardBackend {
-	getEvents() {
-		return new Promise(async (resolve, reject) => {
-			try {
-				const res = await axios.get(`${Constants.API_URL}/events`, {
-					headers: {
-						Authorization: `Bearer ${await AsyncStorage.getItem(
-							"session_token"
-						)}`,
-					},
-				});
-				if (res.data.code !== 200) throw res.data;
-				resolve(res.data);
-			} catch (error) {
-				reject(error);
-			}
-		});
-	}
+      getEvents() {
+            return new Promise(async (resolve, reject) => {
+                  try {
+                        const res = await axios.get(`${Constants.API_URL}/events`, {
+                              headers: {
+                                    Authorization: `Bearer ${await AsyncStorage.getItem("session_token")}`,
+                              },
+                        });
+                        if (res.data.code !== 200) throw res.data;
+                        resolve(res.data);
+                  } catch (error) {
+                        reject(error);
+                  }
+            });
+      }
 }
 
-export default dashboardBackend = new DashboardBackend();
+export let dashboardBackend = new DashboardBackend();
